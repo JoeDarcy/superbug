6e85adbd1dad5b2d792204d6ec74993fa28fcb3b
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2020-12-07 02:42:54 +0000

    Furneral cut scene completed

---
+++ Superbug/Assets/Scripts/FuneralCutSceneScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FuneralCutSceneScript : MonoBehaviour
+{
+    public GameObject church;
+    public GameObject deathCloseup;
+    public GameObject canvas;
+
+    private float timer = 0f;
+
+    // Update is called once per frame
+    void Update() {
+        timer += Time.deltaTime;
+
+        if (timer >= 4) {
+            church.SetActive(false);
+        }
+
+        if (timer >= 8) {
+            deathCloseup.SetActive(false);
+        }
+
+        if (timer >= 12) {
+            canvas.SetActive(true);
+        }
+
+        if (timer >= 20) {
+            Application.Quit();
+        }
+    }
+}
