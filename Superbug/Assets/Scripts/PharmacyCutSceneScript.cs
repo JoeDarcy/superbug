134307764c56edcf37404058ca5a34979c4ea4af
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2020-12-07 01:15:44 +0000

    Pharmacy scene started

---
+++ Superbug/Assets/Scripts/PharmacyCutSceneScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PharmacyCutSceneScript : MonoBehaviour
+{
+    public GameObject external;
+    public GameObject host;
+
+    private float timer = 0f;
+
+    // Update is called once per frame
+    void Update() {
+        timer += Time.deltaTime;
+
+        if (timer >= 3) {
+            external.SetActive(false);
+        }
+
+        if (timer >= 3) {
+            
+        }
+
+        if (timer >= 9) {
+            SceneManager.LoadScene("Level_2_Scene");
+        }
+    }
+}
